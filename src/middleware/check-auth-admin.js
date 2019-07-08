ca7aa63b8ef51cca682bb3cd0c8528198e6c508b
mitu <mitu2687sp@gmail.com>
2019-07-08 15:33:15 +0900

    modified middleware

--- src/middleware/check-auth-admin.js
+++ src/middleware/check-auth-admin.js
@@ -5,7 +5,9 @@ export default async (to, from, next) => {
   if (!store.getters[getter.ADMIN_CHECK] && store.getters[getter.ADMIN_TOKEN]) {
     try {
       await store.dispatch(action.FETCH_ADMIN_USER)
-    } catch (e) { }
+    } catch (e) {
+        store.dispatch(action.ADMIN_LOGOUT)
+    }
   }
 
   next()
