3c562cbd12dc0d03afa7a8ac1ab42b86036ec3ca
mitu <mitu2687sp@gmail.com>
2019-07-08 18:28:22 +0900

    BootstrapVue 補完対策

--- src/main.js
+++ src/main.js
@@ -5,16 +5,14 @@ import 'core-js/es6/string'
 import 'core-js/es7/array'
 // import cssVars from 'css-vars-ponyfill'
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 import store from './store'
 import './plugins'
+import 'BootstrapVueMap.js'
 
 // todo
 // cssVars()
-
-Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
